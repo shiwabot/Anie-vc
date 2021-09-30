6aaeca9b2422adea5aac2920a3e721dc34c05336
Zaid <83804937+Itsunknown-12@users.noreply.github.com>
2021-09-30 14:22:46 +0530

    Update play.py

--- codes/play.py
+++ codes/play.py
@@ -201,7 +201,7 @@ async def play(_, message: Message):
             )
         except Exception as e:
             title = "NaN"
-            thumb_name = "https://telegra.ph/file/638c20c44ca418c8b2178.jpg"
+            thumb_name = "https://telegra.ph/file/b5f0915e87f5049a90efd.jpg"
             duration = "NaN"
             views = "NaN"
             keyboard = InlineKeyboardMarkup(
